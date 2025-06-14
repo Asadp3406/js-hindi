9209371345f441fc847bf0c5095810e4df76572a
Asadp3406 <pathanasad3406@gmail.com>
2025-06-14 17:18:38 +0000

    data types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+//alert(3+3) we are using node js not browser so it wont work
+console.log(3+
+    3) // code should be redable 
+
+    let name = "Asad"
+    let age = 18 
+    let isloggedin = false 
+
+// number => 2 to power 18 
+ // bigint 
+  // string => " "
+  // boolean 
+  // null => stand alone value 
+  // undefined 
+  //symbol = > unique
+  // object
+
+  console.log(typeof age)
+  console.log(typeof null) // it is object 
