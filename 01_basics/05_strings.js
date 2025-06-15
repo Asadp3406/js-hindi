d8dc3b2954737030f93abb724a3993f0a8384c36
Asadp3406 <pathanasad3406@gmail.com>
2025-06-15 08:31:18 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "asad"
+const repocount = 50
+console.log(name+repocount+"asad");
+console.log(`hello my name is ${name}and my repo count is ${repocount}`);
+
+
+const gameName = new String("asaad")
+
+console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toLowerCase);
+//console.log(gameName.charAt('3'));
+//console.log(gameName.indexOf('a')c
+
+const newstring = gameName.substring(0,3);
+console.log(newstring);
+//last ki value nahi ati 
+
+const anotherstring = gameName.slice(-2,1)
+console.log(anotherstring);
+
+
+const newstrongs = " asad   "
+console.log(newstrongs);
+console.log(newstrongs.trim());
+
+
+//repalce 
+const url = " www.asad. com"
+console.log(url.replace("www","ttt"))
