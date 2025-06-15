edce7a414cf98b6fdb4dc7ffdac129c6cfb2696a
Asadp3406 <pathanasad3406@gmail.com>
2025-06-15 09:36:52 +0000

    maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -33,6 +33,7 @@ console.log(Math.floor(Math.random()*10) +1);
 const min = 10 
 const max = 20
 // remember this formula 
-// range 
+// range git push origin main
+
 
 console.log(Math.floor(Math.random() * (max-min + 1 ))+min)
