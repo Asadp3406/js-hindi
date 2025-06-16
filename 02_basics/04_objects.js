d26ce7873cd6ad59c2262f8651ea8734327e13e8
Asadp3406 <pathanasad3406@gmail.com>
2025-06-16 12:33:17 +0000

    objets s

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,47 @@
+ const tinderuser = new Object()
+tinderuser.id= "asad"
+ tinderuser.name= "adad"
+
+
+// console.log(tinderuser);
+
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+// const obj3 = Object.assign( {},obj1,obj2,obj4)
+// console.log(obj3);
+
+const obj3= {...obj1,...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email;
+//keys are in form of array
+console.log(Object.keys(tinderuser));
