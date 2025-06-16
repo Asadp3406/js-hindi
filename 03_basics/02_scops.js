b28ab343d7170c1584c96c9907481ca24d07403d
Asadp3406 <pathanasad3406@gmail.com>
2025-06-16 15:33:39 +0000

    change

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,57 @@
+let a = 300;
+
+if (true ) {
+    let a =10;
+    const b = 43 ;
+//console.log(a);
+ 
+}
+// // console.log(a);
+// // console.log(b);
+// console.log(a);
+
+
+
+
+function one(){
+   const name = "Asad"
+    function two (){
+        const website="ASHH"
+      ///  console.log(name);
+         
+        
+
+    }
+   // console.log(website);
+    two()
+}
+one()
+
+
+if (true) {
+    const name = "asad"
+    if (name=="asad") {
+        const website = " youtube"
+      //  console.log(name+website);
+        
+        
+    }
+ //   console.log(website);
+    
+}
+//console.log(name);
+
+
+
+// ///////////////// intresting ///////////
+addone(5)
+function addone(num){
+return num+1
+}
+
+
+//we can write function in both wats 
+const addtwo = function(num){
+    return num+2
+}
+addtwo(4)
