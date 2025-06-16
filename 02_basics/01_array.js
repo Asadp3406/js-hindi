58e5625a2ed6c8bb0040269faad5a202ae66cecb
Asadp3406 <pathanasad3406@gmail.com>
2025-06-16 11:49:15 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,18 @@
+const myarr = [1,2,3,4];
+
+console.log(myarr[1]);
+
+
+// // to push the element 
+// // pop 
+// myarr.push(4);
+// myarr.pop();
+// console.log(myarr);
+
+// myarr.unshift(0);
+// console.log(myarr);
+
+//to cheack present or not 
+console.log(myarr.includes(9));
+
+ 
